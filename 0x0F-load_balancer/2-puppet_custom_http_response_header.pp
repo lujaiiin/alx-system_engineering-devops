b9e53d3940d67ff2d2aeb8b5db419a08d14aa064
lujaiiin <lujainben232@gmail.com>
2024-01-29 23:05:32 +0000

    lala

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+exec { 'update':
+  command  => 'sudo apt-get -y update',
+  provider => shell,
+}
+-> exec { 'install':
+  command  => 'sudo apt-get -y install nginx',
+  provider => shell,
+}
+-> exec { 'X-Served-By':
+  command  => 'sudo sed -i "/listen 80 default_server;/a         add_header X-Served-By $(hostname);
+" /etc/nginx/sites-available/default',
+  provider => shell,
+}
+-> exec { 'service -y nginx':
+  command => '/usr/sbin/service nginx restart',
+}
