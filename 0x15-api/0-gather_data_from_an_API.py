d8f05edcfb3c026c4b709d61922fcd1764283a3b
lujain <lujainm.com>
2024-02-19 13:37:20 -0800

    API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Modules"""
+import sys as s
+import re
+import requests
+
+
+API = "https://jsonplaceholder.typicode.com"
+
+if __name__ == '__main__':
+    if len(s.argv) > 1:
+        if re.fullmatch(r'\d+', s.argv[1]):
+            id = int(s.argv[1])
+            req = requests.get('{}/users/{}'.format(API, id)).json()
+            task = requests.get('{}/todos'.format(API)).json()
+            name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task))
+            completed = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    name,
+                    len(completed),
+                    len(tasks)
+                )
+            )
+            if len(completed) > 0:
+                for task in completed:
+                    print('\t {}'.format(task.get('title')))
