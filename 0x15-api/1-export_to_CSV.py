3d19b49647441d9c9c9a68cb4dac0bed461edd15
lujain <lujainm.com>
2024-02-19 13:53:31 -0800

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Modules"""
+import csv
 import requests
 import sys
 
 if __name__ == '__main__':
+    # Convert the employee ID to an integer
     employeeId = int(sys.argv[1])
     baseUrl = "https://jsonplaceholder.typicode.com/users"
     url = baseUrl + "/" + str(employeeId)
@@ -11,12 +13,13 @@ if __name__ == '__main__':
     resp = requests.get(url)
     name = resp.json().get('name')
 
+    # Define the URL for todos with the correct user ID
     todosUrl = "https://jsonplaceholder.typicode.com/todos?userId=" + str(employeeId)
     resp = requests.get(todosUrl)
     tasks = resp.json()
 
-    with open('{}.csv'.format(employeeId), 'w') as file:
+    # Write the tasks to a CSV file
+    with open('{}.csv'.format(employeeId), 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in tasks:
-            file.write('"{}","{}","{}","{}"\n'
-                       .format(employeeId, name, task.get('completed'),
-                               task.get('title')))
+            writer.writerow([employeeId, name, task.get('completed'), task.get('title')])
