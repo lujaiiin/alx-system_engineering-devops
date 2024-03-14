8932096a6e879c2e0f624d65e02fec8c3ee12c42
lujain <lujain.com>
2024-03-14 13:09:38 +0200

    lala

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,13 @@
-# Increase the ULIMIT
- file_line { 'increase_ulimit_for_nginx':
-  ensure => present,
-   path   => '/etc/default/nginx',
-    line   => 'ulimit -n 4096',
-     match => '^ulimit -n',
-     } ->
+# the amount of traffic
 
-     # Restart Nginx
-     exec { 'nginx-restart':
-      command => 'nginx restart',
-       path    => '/usr/sbin:/usr/bin:/sbin:/bin', # Adjust the path as necessary
-        require => File_line['increase_ulimit_for_nginx'],
-        }
+# ulimit
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# restart nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
